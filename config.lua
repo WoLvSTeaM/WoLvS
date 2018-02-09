6e1f98fc01570bb3846a84e159d69acd927dfa03
WoLeSTeaM <36292225+WoLvSTeaM@users.noreply.github.com>
2018-02-09 14:45:34 +0330

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,24 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "BanHammer",
+    "Fun",
+    "GroupManager",
+    "Msg-Checks",
+    "Plugins",
+    "Tools",
+    "Write"
+  },
+  info_text = "\9》WoLvS BoT v5.7\nAn advanced administration bot based on https://valtman.name/telegram-cli\n\n》https://github.com/BeyondTeam/BDReborn \n\n》Admins :\n》@Arashsudo ➣ Founder & Developer《\n》@JavadSudo ➣ Developer《\n》@Shaniloop ➣ Developer《\n\n》Special thanks to :\n》WoLvSTeaM\n》@Xamarin_Devloper\n\n》Our channel :\n》@WoLvSTeam《\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    205549111,
+    205549111,
+    205549111
+  }
+}
+return _
+end
